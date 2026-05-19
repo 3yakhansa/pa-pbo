073d4e908648f115a0c4bf1615620c7fb4ef89fb
avellash <avellash.tik@gmail.com>
2026-05-20 05:11:08 +0800

    dashboard

--- src/main/java/com/example/sistemmanajemenshelterkucingpawpatrol/DashboardAdminController.java
+++ src/main/java/com/example/sistemmanajemenshelterkucingpawpatrol/DashboardAdminController.java
@@ -1,14 +1,26 @@
 package com.example.sistemmanajemenshelterkucingpawpatrol;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class DashboardAdminController {
 
     @FXML
+    private Label menuLabel;
+
+    @FXML
     public void initialize() {
-        // Method ini akan otomatis berjalan saat halaman admin terbuka
         System.out.println("Dashboard Admin Berhasil Dimuat!");
 
-        // Nanti kamu bisa mengambil data dari database shelter di sini
+        String menu = """
+                Login berhasil! Selamat datang
+
+                << Menu Utama Shelter >>
+                1. Kelola Data Kucing
+                2. Kelola Adopsi
+                3. Logout & Keluar
+                """;
+
+        menuLabel.setText(menu);
     }
 }
--- src/main/java/com/example/sistemmanajemenshelterkucingpawpatrol/DashboardPenjagaController.java
+++ src/main/java/com/example/sistemmanajemenshelterkucingpawpatrol/DashboardPenjagaController.java
@@ -1,12 +1,30 @@
 package com.example.sistemmanajemenshelterkucingpawpatrol;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class DashboardPenjagaController {
 
     @FXML
+    private Label labelWelcome;
+
+    @FXML
     public void initialize() {
-        // Method ini akan otomatis berjalan saat halaman penjaga terbuka
-        System.out.println("Dashboard Penjaga Berhasil Dimuat!");
+        labelWelcome.setText("Login berhasil! Selamat datang, Penjaga");
+    }
+
+    @FXML
+    private void handleKelolaKucing() {
+        System.out.println("Menu Kelola Data Kucing diklik");
+    }
+
+    @FXML
+    private void handleKelolaAdopsi() {
+        System.out.println("Menu Kelola Adopsi diklik");
+    }
+
+    @FXML
+    private void handleLogout() {
+        System.out.println("Logout diklik");
     }
 }
